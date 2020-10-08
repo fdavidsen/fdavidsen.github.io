43b52469c6124a75c089daa51696bd2badc75600
fdavidsen <f.davidsen19@gmail.com>
2020-10-08 13:20:21 +0700

    Add URL for mail

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,1 +1,5 @@
+// URL for JSON files.
 const baseURL = 'https://fdavidsen.github.io/';
+
+// URL for mail at contact form.
+const mailURL = 'https://fdavidsen.ml/';
--- core/mail/contact_me.js
+++ core/mail/contact_me.js
@@ -21,7 +21,7 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: baseURL + "core/mail/contact_me.php",
+                url: mailURL + "core/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
