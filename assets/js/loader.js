b0b26765a6493f59d5af0131588bc01c30aa20ea
Frederic <f.davidsen19@gmail.com>
2020-10-23 11:12:21 +0700

    Add box shadow and fix link

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -45,12 +45,16 @@ $.ajax({
             // portfolio.html
             $('.portfolio #portfolio .row').append('<div class="col-lg-4 col-sm-6 mb-4"><div class="portfolio-item"><a class="portfolio-link" data-toggle="modal" href="#portfolioModal' + item.id + '"><div class="portfolio-hover"><div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div></div><img class="img-fluid" src="' + item.picture + '" alt="' + item.name + '" /></a><div class="portfolio-caption"><div class="portfolio-caption-heading">' + item.name + '</div><div class="portfolio-caption-subheading text-muted">' + item.category + '</div></div></div></div>');
             
-            let preview = '';
+            let previewButton = '';
+            let previewProject = '<img class="img-fluid d-block mx-auto" src="' + item.picture + '" alt="' + item.name + '">';
+
             if (item.preview != null) {
-                preview = '<br class="break"><a class="btn btn-primary btn-preview" href="' + item.preview + '" target="_blank"><i class="fas fa-globe mr-2"></i>Project Preview</a>';
+                previewButton = '<br class="break"><a class="btn btn-primary btn-preview" href="' + item.preview + '" target="_blank"><i class="fas fa-globe mr-2"></i>Project Preview</a>';
+
+                previewProject = '<a href="' + item.preview + '" target="_blank">' + previewProject + '</a>';
             }
 
-            $('#portfolio-modal').append('<div class="portfolio-modal modal fade" id="portfolioModal' + item.id + '" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="close-modal" data-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal"></div><div class="container"><div class="row justify-content-center"><div class="col-lg-8"><div class="modal-body"><h2 class="text-uppercase">' + item.name + '</h2><p class="item-intro text-muted">' + item.category + '</p><a href="' + item.picture + '" target="_blank"><img class="img-fluid d-block mx-auto" src="' + item.picture + '" alt="' + item.name + '"></a><p>' + item.description + '</p><ul class="list-inline"><li>Technologies: ' + item.technologies + '</li><li>Author: ' + item.author + '</li></ul><a class="btn btn-dark" href="' + item.github + '" target="_blank"><i class="fab fa-github mr-2"></i>GitHub</a>' + preview + '</div></div></div></div></div></div></div>');
+            $('#portfolio-modal').append('<div class="portfolio-modal modal fade" id="portfolioModal' + item.id + '" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="close-modal" data-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal"></div><div class="container"><div class="row justify-content-center"><div class="col-lg-8"><div class="modal-body"><h2 class="text-uppercase">' + item.name + '</h2><p class="item-intro text-muted">' + item.category + '</p>' + previewProject + '<p>' + item.description + '</p><ul class="list-inline"><li>Technologies: ' + item.technologies + '</li><li>Author: ' + item.author + '</li></ul><a class="btn btn-dark" href="' + item.github + '" target="_blank"><i class="fab fa-github mr-2"></i>GitHub</a>' + previewButton + '</div></div></div></div></div></div></div>');
         });
     }
 });
